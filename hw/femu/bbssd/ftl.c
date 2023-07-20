413201d7f8f8f30277170fba793e7a55e14260c1
dash <2423388741@qq.com>
2023-07-20 19:12:07 +0800

    sucks

--- hw/femu/bbssd/ftl.c
+++ hw/femu/bbssd/ftl.c
@@ -769,7 +769,7 @@ static int do_gc(struct ssd *ssd, bool force)
 
 static uint64_t ssd_read(struct ssd *ssd, NvmeRequest *req)
 {
-    ftl_log("1234567");
+    femu_log("123123414");
     struct ssdparams *spp = &ssd->sp;
     uint64_t lba = req->slba;
     int nsecs = req->nlb;
@@ -861,7 +861,7 @@ static uint64_t ssd_write(struct ssd *ssd, NvmeRequest *req)
 
 static void *ftl_thread(void *arg)
 {
-    ftl_log("123123414");
+    femu_log("123123414");
     FemuCtrl *n = (FemuCtrl *)arg;
     struct ssd *ssd = n->ssd;
     NvmeRequest *req = NULL;
